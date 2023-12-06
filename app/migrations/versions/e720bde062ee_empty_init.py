cf9d333cb0604260ab198ba8f6f1c153cf35fc64
shekinah <nyamburamary1212@gmail.com>
2023-12-06 23:16:25 +0300

    Complete code along migrations and alembic

---
+++ app/migrations/versions/e720bde062ee_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: e720bde062ee
+Revises: 
+Create Date: 2023-12-06 22:53:44.852770
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'e720bde062ee'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
